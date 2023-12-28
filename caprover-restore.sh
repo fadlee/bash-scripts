7f0661d7a7a6567088cb71c128304411a424fc4e
Fadlul Alim <fad.lee@hotmail.com>
2023-12-29 05:03:43 +0700

    fix restore

--- caprover-restore.sh
+++ caprover-restore.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CURRENT_DIR=$(pwd)
+backup_file="/captain/backup.tar"
 
-read -p "Enter Caprover backup.tar directory [default: $CURRENT_DIR]: " INPUT_DIR
-
-BACKUP_PATH="${INPUT_DIR:-$CURRENT_DIR}"
-
-docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v $BACKUP_PATH:/captain caprover/caprover
+if [ -e "$backup_file" ]; then
+    docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v /captain:/captain caprover/caprover
+else
+    echo "Error: $backup_file not exists"
+fi

dc796585b04a5b4f16069bcee7905c2045a4ed05
Fadlul Alim <fad.lee@hotmail.com>
2023-12-29 02:28:49 +0700

    prompt for backup path

--- caprover-restore.sh
+++ caprover-restore.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 CURRENT_DIR=$(pwd)
-docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v $CURRENT_DIR:/captain caprover/caprover
+
+read -p "Enter Caprover backup.tar directory [default: $CURRENT_DIR]: " INPUT_DIR
+
+BACKUP_PATH="${INPUT_DIR:-$CURRENT_DIR}"
+
+docker run -p 80:80 -p 443:443 -p 3000:3000 -e ACCEPTED_TERMS=true -v /var/run/docker.sock:/var/run/docker.sock -v $BACKUP_PATH:/captain caprover/caprover

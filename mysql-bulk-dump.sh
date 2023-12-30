9adffaca2f2c2ab2a28c97add605e280b415f1b3
Fadlul Alim <fad.lee@hotmail.com>
2023-12-30 07:58:06 +0700

    fix: allow empty params

--- mysql-bulk-dump.sh
+++ mysql-bulk-dump.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Prompt the user for MySQL connection details
-read -p "Enter MySQL host: " DB_HOST
-read -p "Enter MySQL username: " DB_USER
-read -s -p "Enter MySQL password: " DB_PASSWORD
+read -p "Enter MySQL host (press Enter for default): " DB_HOST
+read -p "Enter MySQL username (press Enter for default): " DB_USER
+read -s -p "Enter MySQL password (press Enter for default): " DB_PASSWORD
 echo
 
 # Prompt the user for a list of database names (space-separated)
@@ -24,8 +24,11 @@ do
   # Create backup file name
   BACKUP_FILE="$BACKUP_DIR/${DB_NAME}_${DATE}.sql"
 
-  # Perform mysqldump
-  mysqldump -h$DB_HOST -u$DB_USER -p$DB_PASSWORD --databases $DB_NAME > $BACKUP_FILE
+  # Perform mysqldump without -h, -u, -p if the values are empty
+  mysqldump $([ -n "$DB_HOST" ] && echo "-h$DB_HOST") \
+            $([ -n "$DB_USER" ] && echo "-u$DB_USER") \
+            $([ -n "$DB_PASSWORD" ] && echo "-p$DB_PASSWORD") \
+            --databases $DB_NAME > $BACKUP_FILE
 
   # Check if mysqldump was successful
   if [ $? -eq 0 ]; then
